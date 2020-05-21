d64be695dc6432a28173edb309ea675ac2c77e8c
Dinh Thai <dinh.thai@codeenginestudio.com>
2020-05-21 14:42:57 +0700

    add training portlet

---
+++ modules/training-portlet/src/main/java/com/liferay/training/portlet/constants/TrainingMVCPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.portlet.constants;
+
+/**
+ * @author ces-user
+ */
+public class TrainingMVCPortletKeys {
+
+	public static final String TRAININGMVC =
+		"com_liferay_training_portlet_TrainingMVCPortlet";
+
+}
